33689b2a471f9e9933957d371fc1ad28c2afcc40
Jake Shilling <shilling.jake@gmail.com>
2024-06-10 20:53:59 -0400

    Initial commit

---
+++ saplingai.el
@@ -0,0 +1,25 @@
+;;; saplingai.el --- Integrate Emacs with the Sapling.ai Spelling/Grammar checker  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Functor Factory
+;;
+;; Author: Jake Shilling <jshilling@functorfactory.com>
+;; Maintainer: Jake Shilling <jshilling@functorfactory.com>
+;; Created: June 10, 2024
+;; Modified: June 10, 2024
+;; Version: 0.0.1
+;; Keywords: grammar spelling sapling
+;; Homepage: https://github.com/FunctorFactory/saplingai.el
+;; Package-Requires: ((emacs "26.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'saplingai)
+;;; saplingai.el ends here
